cee755b4acc559a137ce2c6dbebb23ed2c80f242
quanrb <124875538+quanrb@users.noreply.github.com>
2023-04-09 11:34:38 +0700

    Add files via upload

---
+++ include/groundtile.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <vector>
+
+#include "entity.h"
+using namespace std;
+
+class GroundTile : public Entity
+{
+public:
+	GroundTile(SDL_Texture* p_tex, int p_index);
+	int getStatus();
+	void setStatus(int p_status, SDL_Texture* groundTex[4]);
+private:
+	int status;
+};
